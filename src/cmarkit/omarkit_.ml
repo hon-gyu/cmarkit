d59175a38574f15e4630902b4806c666592b9a15
hongyu <linerwang1997@gmail.com>
2026-05-20 23:40:09 +0100

    extract internal mod for testing + pp

---
+++ src/cmarkit/omarkit_.ml
@@ -0,0 +1,263 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2021 The cmarkit programmers. All rights reserved.
+   SPDX-License-Identifier: ISC
+  ---------------------------------------------------------------------------*)
+
+module String_map = Map.Make (String)
+module Ascii = Cmarkit_base.Ascii
+module Text = Cmarkit_base.Text
+module Match = Cmarkit_base
+module Textloc = Cmarkit_base.Textloc
+module Meta = Cmarkit_base.Meta
+module Layout = struct
+  type blanks = string
+  type nonrec string = string
+  type nonrec char = char
+  type count = int
+  type indent = int
+  let string ?(meta = Meta.none) s = s, meta
+  let empty = string ""
+end
+module Pp = Pp
+
+type byte_pos = Textloc.byte_pos
+type line_span = Match.line_span =
+  (* Substring on a single line, hereafter abbreviated to span *)
+  { line_pos : Textloc.line_pos; first : byte_pos; last : byte_pos }
+
+type 'a node = 'a * Meta.t
+
+module Block_line = Common.Block_line
+module Label = Common.Label
+module Link_definition = Common.Link_definition
+
+module Inline = Inline
+
+(* Blocks *)
+
+module Block = Block
+
+(* Parsing *)
+
+include Parser
+
+(* Documents *)
+
+module Doc = struct
+  type t = { nl : Layout.string; block : Block.t; defs : Label.defs }
+  let make ?(nl = "\n") ?(defs = Label.Map.empty) block = { nl; block; defs }
+  let empty = make (Block.Blocks ([], Meta.none))
+  let nl d = d.nl
+  let block d = d.block
+  let defs d = d.defs
+  let of_string
+      ?defs ?resolver ?nested_links ?heading_auto_ids ?layout ?locs ?file
+      ?(strict = true) s
+    =
+    let p =
+      parser ?defs ?resolver ?nested_links ?heading_auto_ids ?layout ?locs
+        ?file ~strict s
+    in
+    let nl, doc = Block_struct.parse p in
+    let block = block_struct_to_doc p doc in
+    make ~nl block ~defs:p.defs
+
+  let unicode_version = Cmarkit_data.unicode_version
+  let commonmark_version = "0.31.2"
+end
+
+(* Maps and folds *)
+
+module Mapper = struct
+  type 'a filter_map = 'a option
+  type 'a result = [ `Default | `Map of 'a filter_map ]
+  let default = `Default
+  let delete = `Map None
+  let ret v = `Map (Some v)
+
+  type t =
+    { inline_ext_default : Inline.t map;
+      block_ext_default : Block.t map;
+      inline : Inline.t mapper;
+      block : Block.t mapper }
+  and 'a map = t -> 'a -> 'a filter_map
+  and 'a mapper = t -> 'a -> 'a result
+
+  let none _ _ = `Default
+  let ext_inline_none _ _ = invalid_arg Inline.err_unknown
+  let ext_block_none _ _ = invalid_arg Block.err_unknown
+  let make
+      ?(inline_ext_default = ext_inline_none)
+      ?(block_ext_default = ext_block_none)
+      ?(inline = none) ?(block = none) ()
+    =
+    { inline_ext_default; block_ext_default; inline; block }
+
+  let inline_mapper m = m.inline
+  let block_mapper m = m.block
+  let inline_ext_default m = m.inline_ext_default
+  let block_ext_default m = m.block_ext_default
+
+  let ( let* ) = Option.bind
+
+  let rec map_inline m i = match m.inline m i with
+  | `Map i -> i
+  | `Default ->
+      let open Inline in
+      match i with
+      | Autolink _ | Break _ | Code_span _ | Raw_html _
+      | Text _ | Ext_math_span _ as i -> Some i
+      | Image (l, meta) ->
+          let text = Option.value ~default:Inline.empty (map_inline m l.text) in
+          Some (Image ({ l with text }, meta))
+      | Link (l, meta) ->
+          let* text = map_inline m l.text in
+          Some (Link ({ l with text }, meta))
+      | Emphasis (e, meta) ->
+          let* inline = map_inline m e.inline in
+          Some (Emphasis ({ e with inline }, meta))
+      | Strong_emphasis (e, meta) ->
+          let* inline = map_inline m e.inline in
+          Some (Strong_emphasis ({ e with inline}, meta))
+      | Inlines (is, meta) ->
+          (match List.filter_map (map_inline m) is with
+          | [] -> None | is -> Some (Inlines (is, meta)))
+      | Ext_strikethrough (s, meta) ->
+          let* inline = map_inline m s in
+          Some (Ext_strikethrough (inline, meta))
+      | ext -> m.inline_ext_default m ext
+
+  let rec map_block m b = match m.block m b with
+  | `Map b -> b
+  | `Default ->
+      let open Block in
+      match b with
+      | Blank_line _ | Code_block _ | Html_block _
+      | Link_reference_definition _ | Thematic_break _
+      | Ext_math_block _ as b -> Some b
+      | Heading (h, meta) ->
+          let inline = match map_inline m (Block.Heading.inline h) with
+          | None -> (* Can be empty *) Inline.Inlines ([], Meta.none)
+          | Some i -> i
+          in
+          Some (Heading ({ h with inline}, meta))
+      | Block_quote (b, meta) ->
+          let block = match map_block m b.block with
+          | None -> (* Can be empty *) Blocks ([], Meta.none) | Some b -> b
+          in
+          Some (Block_quote ({ b with block}, meta))
+      | Blocks (bs, meta) ->
+          (match List.filter_map (map_block m) bs with
+          | [] -> None | bs -> Some (Blocks (bs, meta)))
+      | List (l, meta) ->
+          let map_list_item m (i, meta) =
+            let* block = map_block m (List_item.block i) in
+            Some ({ i with block }, meta)
+          in
+          (match List.filter_map (map_list_item m) l.items with
+          | [] -> None | items -> Some (List ({ l with items }, meta)))
+      | Paragraph (p, meta) ->
+          let* inline = map_inline m (Paragraph.inline p) in
+          Some (Paragraph ({ p with inline }, meta))
+      | Ext_table (t, meta) ->
+          let map_col m (i, layout) = match map_inline m i with
+          | None -> (Inline.empty, layout) | Some i -> (i, layout)
+          in
+          let map_row (((r, meta), blanks) as row) = match r with
+          | `Header is -> (`Header (List.map (map_col m) is), meta), blanks
+          | `Sep _ -> row
+          | `Data is -> (`Data (List.map (map_col m) is), meta), blanks
+          in
+          let rows = List.map map_row t.rows in
+          Some (Ext_table ({ t with Table.rows }, meta))
+      | Ext_footnote_definition (fn, meta) ->
+          let block = match map_block m fn.block with
+          | None -> (* Can be empty *) Blocks ([], Meta.none) | Some b -> b
+          in
+          Some (Ext_footnote_definition ({ fn with block}, meta))
+      | ext -> m.block_ext_default m ext
+
+  let map_doc m d =
+    let map_block m b = Option.value ~default:Block.empty (map_block m b) in
+    (* XXX something better for defs should be devised here. *)
+    let map_def m = function
+    | Block.Footnote.Def (fn, meta) ->
+        let block = map_block m (Block.Footnote.block fn) in
+        Block.Footnote.Def ({ fn with block }, meta)
+    | def -> def
+    in
+    let block = map_block m (Doc.block d) in
+    let defs = Label.Map.map (map_def m) (Doc.defs d) in
+    { d with Doc.block; defs }
+end
+
+module Folder = struct
+  type 'a result = [ `Default | `Fold of 'a ]
+  let default = `Default
+  let ret v = `Fold v
+
+  type ('a, 'b) fold = 'b t -> 'b -> 'a -> 'b
+  and ('a, 'b) folder = 'b t -> 'b -> 'a -> 'b result
+  and 'a t =
+    { inline_ext_default : (Inline.t, 'a) fold;
+      block_ext_default : (Block.t, 'a) fold;
+      inline : (Inline.t, 'a) folder;
+      block : (Block.t, 'a) folder; }
+
+  let none _ _ _ = `Default
+  let ext_inline_none _ _ _ = invalid_arg Inline.err_unknown
+  let ext_block_none _ _ _ = invalid_arg Block.err_unknown
+  let make
+      ?(inline_ext_default = ext_inline_none)
+      ?(block_ext_default = ext_block_none)
+      ?(inline = none) ?(block = none) ()
+    =
+    { inline_ext_default; block_ext_default; inline; block }
+
+  let inline_folder f = f.inline
+  let block_folder f = f.block
+  let inline_ext_default f = f.inline_ext_default
+  let block_ext_default f = f.block_ext_default
+
+  let rec fold_inline f acc i = match f.inline f acc i with
+  | `Fold acc -> acc
+  | `Default ->
+      let open Inline in
+      match i with
+      | Autolink _ | Break _ | Code_span _ | Raw_html _ | Text _
+      | Ext_math_span _ -> acc
+      | Image (l, _) | Link (l, _) -> fold_inline f acc l.text
+      | Emphasis ({ inline }, _) -> fold_inline f acc inline
+      | Strong_emphasis ({ inline }, _) -> fold_inline f acc inline
+      | Inlines (is, _) -> List.fold_left (fold_inline f) acc is
+      | Ext_strikethrough (inline, _) -> fold_inline f acc inline
+  | ext -> f.inline_ext_default f acc ext
+
+  let rec fold_block f acc b = match f.block f acc b with
+  | `Fold acc -> acc
+  | `Default ->
+      let open Block in
+      match b with
+      | Blank_line _ | Code_block _ | Html_block _
+      | Link_reference_definition _ | Thematic_break _ | Ext_math_block _ -> acc
+      | Heading (h, _) -> fold_inline f acc (Block.Heading.inline h)
+      | Block_quote (bq, _) -> fold_block f acc bq.block
+      | Blocks (bs, _) -> List.fold_left (fold_block f) acc bs
+      | List (l, _) ->
+          let fold_list_item m acc (i, _) =
+            fold_block m acc (Block.List_item.block i)
+          in
+          List.fold_left (fold_list_item f) acc l.items
+      | Paragraph (p, _) -> fold_inline f acc (Block.Paragraph.inline p)
+      | Ext_table (t, _) ->
+          let fold_row acc ((r, _), _) = match r with
+          | (`Header is | `Data is) ->
+              List.fold_left (fun acc (i, _) -> fold_inline f acc i) acc is
+          | `Sep _ -> acc
+          in
+          List.fold_left fold_row acc t.Table.rows
+      | Ext_footnote_definition (fn, _) -> fold_block f acc fn.block
+      | ext -> f.block_ext_default f acc ext
+
+  let fold_doc f acc d = fold_block f acc (Doc.block d)
+end
---
+++ src/cmarkit/pp.ml
@@ -0,0 +1,102 @@
+(* {0 Pretty-printers}
+
+   show structure and a preview of inline content, dropping [Meta.t] and
+   most layout. Use [~ext] to handle user-defined [Block.t] extensions. *)
+
+open Common
+open Block
+
+let h_epls = "…"
+
+module Listchar = struct
+  let tab = "→ "
+  let trail = "·"
+  let eol = "↵"
+  let nbsp = "␣"
+  let extends = "»"
+  let precedes = "«"
+  let leadmultispace = "│ "
+end
+
+let pp_ext_default _ppf _b = Format.pp_print_string _ppf "<ext>"
+
+let pp_inline_preview ?ext_inline ppf inline =
+  let text = Inline.to_plain_text ?ext:ext_inline ~break_on_soft:true inline in
+  let s = String.concat " " (List.map (String.concat "") text) in
+  let max_len = 60 in
+  let len = String.length s in
+  let s =
+    if len <= max_len then s
+    else
+    Printf.sprintf "%s%s<%dc>" (String.sub s 0 max_len) h_epls (len - max_len)
+  in
+  Format.fprintf ppf "%S" s
+
+(* Commonmark label matching is case-insensitive and whitespace-collapsing *)
+let pp_label ppf l = Format.fprintf ppf "%S" (Label.key l)
+
+let rec pp_block_with ?(ext = pp_ext_default) ?ext_inline () ppf = function
+| Blank_line _ -> Format.pp_print_string ppf "Blank_line"
+| Block_quote (bq, _) ->
+    Format.fprintf ppf "@[<v 2>Block_quote@,%a@]" (pp_block_with ~ext ?ext_inline ())
+      (Block_quote.block bq)
+| Blocks ([], _) -> Format.pp_print_string ppf "Blocks []"
+| Blocks (bs, _) ->
+    Format.fprintf ppf "@[<v 2>Blocks@,%a@]"
+      (Format.pp_print_list ~pp_sep:Format.pp_print_cut (pp_block_with ~ext ?ext_inline ()))
+      bs
+| Code_block (cb, _) ->
+    let name =
+      match Code_block.layout cb with
+      | `Indented -> "Indented_code_block"
+      | `Fenced _ -> "Fenced_code_block"
+    in
+    let info =
+      match Code_block.info_string cb with
+      | None -> "-"
+      | Some (s, _) -> Printf.sprintf "%S" s
+    in
+    Format.fprintf ppf "%s { info=%s; lines=%d }" name info
+      (List.length (Code_block.code cb))
+| Heading (h, _) ->
+    Format.fprintf ppf "@[<2>Heading H%d@ %a@]" (Heading.level h)
+      (pp_inline_preview ?ext_inline)
+      (Heading.inline h)
+| Html_block (lines, _) ->
+    Format.fprintf ppf "Html_block { lines=%d }" (List.length lines)
+| Link_reference_definition (ld, _) ->
+    let label =
+      match Link_definition.label ld with
+      | None -> "-"
+      | Some l -> Printf.sprintf "%S" (Label.key l)
+    in
+    Format.fprintf ppf "Link_reference_definition { label=%s }" label
+| List (l, _) ->
+    let type_str =
+      match List'.type' l with
+      | `Unordered c -> Printf.sprintf "unordered %C" c
+      | `Ordered (n, c) -> Printf.sprintf "ordered %d%C" n c
+    in
+    let tight = if List'.tight l then "tight" else "loose" in
+    Format.fprintf ppf "@[<v 2>List { %s; %s }@,%a@]" type_str tight
+      (Format.pp_print_list ~pp_sep:Format.pp_print_cut (fun ppf (i, _) ->
+           Format.fprintf ppf "@[<v 2>- item@,%a@]" (pp_block_with ~ext ?ext_inline ())
+             (List_item.block i)))
+      (List'.items l)
+| Paragraph (p, _) ->
+    Format.fprintf ppf "@[<2>Paragraph@ %a@]"
+      (pp_inline_preview ?ext_inline)
+      (Paragraph.inline p)
+| Thematic_break _ -> Format.pp_print_string ppf "Thematic_break"
+| Ext_math_block (cb, _) ->
+    Format.fprintf ppf "Ext_math_block { lines=%d }"
+      (List.length (Code_block.code cb))
+| Ext_table (t, _) ->
+    Format.fprintf ppf "Ext_table { cols=%d; rows=%d }" (Table.col_count t)
+      (List.length (Table.rows t))
+| Ext_footnote_definition (fn, _) ->
+    Format.fprintf ppf "@[<v 2>Ext_footnote_definition %a@,%a@]" pp_label
+      (Footnote.label fn) (pp_block_with ~ext ?ext_inline ()) (Footnote.block fn)
+| b -> ext ppf b
+
+let pp_block = pp_block_with ()
